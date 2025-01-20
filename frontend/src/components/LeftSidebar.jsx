73ef82ea7a8d16a2bd38aa97aaf40c415992aad6
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-20 23:49:37 +0000

    Add notification and message icons to LeftSidebar component

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import logo from '../../public/logo.svg'
 import { GoHomeFill } from "react-icons/go";
 import { IoSearch } from "react-icons/io5";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FaRegEnvelope } from "react-icons/fa";
+
+
 
 
 const LeftSidebar = () => {
@@ -14,39 +18,39 @@ const LeftSidebar = () => {
                     </a>
                 </div>
                 <div className='text-white text-2xl py-6'>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <GoHomeFill className='text-3xl' />
                         <h2>Home</h2>
                     </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <IoSearch className='text-3xl' />
                         <h2>Explore</h2>
                     </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
+                        <IoNotificationsOutline className='text-3xl' />
+                        <h2>Notifications</h2>
+                    </div>
+                    <div className='flex gap-5 cursor-pointer py-6'>
+                        <FaRegEnvelope className='text-3xl' />
+                        <h2>Messages</h2>
+                    </div>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <GoHomeFill className='text-3xl' />
                         <h2>Home</h2>
                     </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <GoHomeFill className='text-3xl' />
                         <h2>Home</h2>
                     </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <GoHomeFill className='text-3xl' />
                         <h2>Home</h2>
                     </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <GoHomeFill className='text-3xl' />
                         <h2>Home</h2>
                     </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
-                        <GoHomeFill className='text-3xl' />
-                        <h2>Home</h2>
-                    </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
-                        <GoHomeFill className='text-3xl' />
-                        <h2>Home</h2>
-                    </div>
-                    <div className='flex gap-2 cursor-pointer py-6'>
+                    <div className='flex gap-5 cursor-pointer py-6'>
                         <button className='bg-white text-black text-lg font-semibold px-24 py-3 rounded-full'>Post</button>
                     </div>
                 </div>
