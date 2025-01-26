77e6ecbc1ee8d5a5c0645611e697955bd5c4eb07
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-26 21:16:35 +0000

    Refactor Tweet component and add like/dislike and delete functionality

--- frontend/src/components/Tweet.jsx
+++ frontend/src/components/Tweet.jsx
@@ -1,70 +1,94 @@
-import React from 'react'
-import Avatar from 'react-avatar'
+import React from 'react';
+import Avatar from "react-avatar";
 import { FaRegComment } from "react-icons/fa";
-import { BiRepost } from "react-icons/bi";
+import { MdOutlineDeleteOutline } from "react-icons/md";
 import { CiHeart } from "react-icons/ci";
-import { FiBarChart2 } from "react-icons/fi";
 import { CiBookmark } from "react-icons/ci";
-import { FiShare } from "react-icons/fi";
+import axios from "axios";
+import { TWEET_API_END_POINT } from '../utils/constant';
+import toast from "react-hot-toast";
+import { useSelector, useDispatch } from "react-redux";
+import { getRefresh } from '../redux/tweetSlice';
+import { timeSince } from "../utils/constant";
 
+const Tweet = ({ tweet }) => {
+    const { user } = useSelector(store => store.user);
 
+    const dispatch = useDispatch();
+    const likeOrDislikeHandler = async (id) => {
+        try {
+            const res = await axios.put(`${TWEET_API_END_POINT}/like/${id}`, { id: user?._id }, {
+                withCredentials: true
+            })
+            console.log(res);
+            dispatch(getRefresh());
+            toast.success(res.data.message);
 
-
-
-
-const Tweet = () => {
+        } catch (error) {
+            toast.success(error.response.data.message);
+            console.log(error);
+        }
+    }
+    const deleteTweetHandler = async (id) => {
+        try {
+            axios.defaults.withCredentials = true;
+            const res = await axios.delete(`${TWEET_API_END_POINT}/delete/${id}`);
+            console.log(res);
+            dispatch(getRefresh());
+            toast.success(res.data.message);
+        } catch (error) {
+            toast.success(error.response.data.message);
+            console.log(error);
+        }
+    }
     return (
-        <div className='border-b border-stone-800'>
-            <div className='flex p-4'>
-                <Avatar src='https://pbs.twimg.com/profile_images/1448239823/IMG_0001_400x400.jpg' size='40' round={true} />
-                <div className='ml-2'>
-                    <div className='flex items-center'>
-                        <h1 className='font-bold'>Name</h1>
-                        <p className='text-gray-500 text-sm ml-1'>@username</p>
-                    </div>
-                    <div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti maiores, eum ut vitae doloribus beatae delectus animi consequuntur neque eaque similique repellat, omnis velit. Facere minus facilis blanditiis dolorum eos!</p>
-                    </div>
-                    <div className='flex justify-between'>
-                        <div className='flex items-center  cursor-pointer'>
-                            <div className='p-2 text-gray-500  hover:text-blue-500'>
-                                <FaRegComment size="20px" />
-                            </div>
-                            <p className='ml-2'>0</p>
+        <div className='border-b border-gray-200'>
+            <div>
+                <div className='flex p-4'>
+                    <Avatar src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg" size="40" round={true} />
+                    <div className=' ml-2 w-full'>
+                        <div className='flex items-center'>
+                            <h1 className='font-bold'>{tweet?.userDetails[0]?.name}</h1>
+                            <p className='text-gray-500 text-sm ml-1'>{`@${tweet?.userDetails[0]?.username} . ${timeSince(tweet?.createdAt)}`}</p>
                         </div>
-                        <div className='flex items-center cursor-pointer'>
-                            <div className='p-2 text-gray-500 hover:text-green-500'>
-                                <BiRepost size="24px" />
-                            </div>
-                            <p className='ml-2'>0</p>
+                        <div>
+                            <p>{tweet?.description}</p>
                         </div>
-                        <div className='flex items-center cursor-pointer'>
-                            <div className='p-2 text-gray-500 hover:text-red-500'>
-                                <CiHeart size="24px" />
+                        <div className='flex justify-between my-3'>
+                            <div className='flex items-center'>
+                                <div className='p-2 hover:bg-green-200 rounded-full cursor-pointer'>
+                                    <FaRegComment size="20px" />
+                                </div>
+                                <p>0</p>
                             </div>
-                            <p className='ml-2'>0</p>
+                            <div className='flex items-center'>
+                                <div onClick={() => likeOrDislikeHandler(tweet?._id)} className='p-2 hover:bg-pink-200 rounded-full cursor-pointer'>
+                                    <CiHeart size="24px" />
 
-                        </div>
-                        <div className='flex items-center cursor-pointer'>
-                            <div className='p-2 text-gray-500 hover:text-blue-500'>
-                                <FiBarChart2 size="24px" />
+                                </div>
+                                <p>{tweet?.like?.length}</p>
                             </div>
-                            <p className='ml-2'>0</p>
-                        </div>
-                        <div className='flex items-center cursor-pointer'>
-                            <div className='p-2 text-gray-500 hover:text-blue-500'>
-                                <CiBookmark size="20px" />
+                            <div className='flex items-center'>
+                                <div className='p-2 hover:bg-yellow-200 rounded-full cursor-pointer'>
+                                    <CiBookmark size="24px" />
+                                </div>
+                                <p>0</p>
                             </div>
-                        </div>
-                        <div className='flex items-center cursor-pointer'>
-                            <div className='p-2 text-gray-500 hover:text-blue-500'>
-                                <FiShare size="20px" />
-                            </div>
+                            {
+                                user?._id === tweet?.userId && (
+                                    <div onClick={() => deleteTweetHandler(tweet?._id)} className='flex items-center'>
+                                        <div className='p-2 hover:bg-red-300 rounded-full cursor-pointer'>
+                                            <MdOutlineDeleteOutline size="24px" />
+                                        </div>
+                                    </div>
+                                )
+                            }
+
                         </div>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     )
 }
 
