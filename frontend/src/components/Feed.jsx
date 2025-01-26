d8596ad306e625355576729cc75c99a3d5346d68
SaroshAhmad <ahmadsarosh07@gmail.com>
2025-01-26 20:56:54 +0000

    Refactor Feed component to render tweets dynamically

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,11 +3,14 @@ import CreatePost from './CreatePost'
 import Tweet from './Tweet'
 
 const Feed = () => {
+    const { tweets } = useSelector(store => store.tweets);
     return (
         <div className="w-full lg:w-[600px] min-h-screen border-x border-zinc-800">
             <div className="sticky top-0 z-10 backdrop-blur-xl bg-black/70">
                 <CreatePost />
-                <Tweet />
+                {
+                    tweets?.map((tweet) => <Tweet key={tweet?.id} tweet={tweet} />)
+                }
             </div>
             {/* Add posts here */}
         </div>
